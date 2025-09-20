b431e12d12a068bbdeefdbb3ad608d06fec962d1
ekorium <231641293+ekorium@users.noreply.github.com>
2025-09-20 23:07:25 +0200

    test: update test demo

--- script.js
+++ script.js
@@ -9,40 +9,42 @@ function stringifyState(state) {
     return string
 }
 
-const info = document.createElement('div')
-info.id = 'info'
-info.innerHTML = stringifyState(ControlStick.DEFAULT_STATE)
-const stick = new ControlStick({useEvents: true, maxzone: 1.5, knobzone: 1.2})
-stick.div.id = 'stick'
-const knob = stick.createKnob()
-knob.id = 'knob'
-const player = document.createElement('div')
-player.id = 'player'
+window.addEventListener('load', () => {
 
-window.addEventListener('load', function () {
+    const info = document.createElement('div')
+    const stick = new ControlStick({useEvents: true, maxzone: 1.5, knobzone: 1.2})
+    const knob = stick.createKnob()
+    const player = document.createElement('div')
+    
+    info.innerHTML = stringifyState(ControlStick.DEFAULT_STATE)
+    info.id = 'info'
+    stick.div.id = 'stick'
+    knob.id = 'knob'
+    player.id = 'player'
+
     document.body.appendChild(info)
     document.body.appendChild(stick.div)
     document.body.appendChild(player)
-})
 
-function render() {
-    player.style.transform = `translate(${playerX}px, ${playerY}px)`
-}
+    function render() {
+        player.style.transform = `translate(${playerX}px, ${playerY}px)`
+    }
 
-let playerX = 100
-let playerY = 150
-render()
+    let playerX = 100
+    let playerY = 150
+    render()
 
-stick.on('stickmove', (state) => {
-    info.innerHTML = stringifyState(state)
-    const {nx, ny, magnitude} = state
-    playerX += 4 * nx * magnitude
-    playerY += 4 * ny * magnitude
-    playerX = utils.clamp(playerX, 0, window.innerWidth - 100)
-    playerY = utils.clamp(playerY, 0, window.innerHeight - 100)
-    requestAnimationFrame(render)
-})
+    stick.on('stickmove', (state) => {
+        info.innerHTML = stringifyState(state)
+        const {nx, ny, magnitude} = state
+        playerX += 4 * nx * magnitude
+        playerY += 4 * ny * magnitude
+        playerX = utils.clamp(playerX, 0, window.innerWidth - 100)
+        playerY = utils.clamp(playerY, 0, window.innerHeight - 100)
+        requestAnimationFrame(render)
+    })
 
-stick.on('stickup', (state) => {
-    info.innerHTML = stringifyState(state)
+    stick.on('stickup', (state) => {
+        info.innerHTML = stringifyState(state)
+    })
 })
